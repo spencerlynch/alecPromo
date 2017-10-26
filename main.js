8469bfdf80fed648d3ff719de208cb56759cdc07
[spencer lynch] <[spencer.lynch9@yahoo.com]>
2017-10-25 23:56:44 -0400

    promo page launched

---
+++ main.js
@@ -0,0 +1,21 @@
+function initMap() {
+  var uluru = {lat: 38.056475, lng: -84.481393};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 17,
+    center: uluru
+  });
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
+		//Resize Function
+		google.maps.event.addDomListener(window, "resize", function() {
+			var center = map.getCenter();
+			google.maps.event.trigger(map, "resize");
+			map.setCenter(center);
+		});
+	
+
+}
+
+
